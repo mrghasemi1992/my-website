d719f55721265ebb149ef7b8a3a0b68e773ec9b3
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2020-11-16 00:47:04 +0330

    A little UI bug in Background.js resolved * Plus about content replaced * And MySkills.js extra padding removed

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,9 +3,9 @@ import email from './email.svg';
 import emailWhite from './email-white.svg';
 import github from './github.svg';
 import githubWhite from './github-white.svg';
-import graduationCap from './graduation-cap.svg';
 import linkedin from './linkedin.svg';
 import linkedinWhite from './linkedin-white.svg';
+import mortarboard from './mortarboard.svg';
 import telegram from './telegram.svg';
 import telegramWhite from './telegram-white.svg';
 
@@ -15,9 +15,9 @@ export {
   emailWhite,
   github,
   githubWhite,
-  graduationCap,
   linkedin,
   linkedinWhite,
+  mortarboard,
   telegram,
   telegramWhite,
 };
