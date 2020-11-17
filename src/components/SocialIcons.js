1fb333bdb32ece37e6911273e2f2e3648bef9205
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2020-11-17 22:48:33 +0330

    Social icons transition removed due to firefox bad transition!

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -17,17 +17,6 @@ const IconsWrapper = styled.div`
     props.direction === 'vertical' ? 'height: 145px' : 'width: 160px'}
 `;
 
-const Icon = styled.a`
-  img {
-    transition: transform 0.2s;
-  }
-  &:hover {
-    img {
-      transform: scale(1.2);
-    }
-  }
-`;
-
 type Props = {
   color: 'egyptianBlue' | 'white',
   direction: 'horizontal' | 'vertical',
@@ -43,37 +32,34 @@ function SocialIcons(props: Props): React$Node {
         direction === 'vertical' && 'flex-column'
       } justify-content-between align-items-center`}
     >
-      <Icon href="mailto: mrghasemi1992@gmail.com" className="pointer">
+      <a href="mailto: mrghasemi1992@gmail.com" className="pointer">
         <img
           src={color === 'egyptianBlue' ? email : emailWhite}
           alt="Email"
           width={25}
         />
-      </Icon>
-      <Icon
-        href="https://www.linkedin.com/in/mrghasemi1992/"
-        className="pointer"
-      >
+      </a>
+      <a href="https://www.linkedin.com/in/mrghasemi1992/" className="pointer">
         <img
           src={color === 'egyptianBlue' ? linkedin : linkedinWhite}
           alt="Linkedin"
           width={25}
         />
-      </Icon>
-      <Icon href="https://t.me/mrghasemi1992" className="pointer">
+      </a>
+      <a href="https://t.me/mrghasemi1992" className="pointer">
         <img
           src={color === 'egyptianBlue' ? telegram : telegramWhite}
           alt="Telegram"
           width={25}
         />
-      </Icon>
-      <Icon href="https://github.com/mrghasemi1992" className="pointer">
+      </a>
+      <a href="https://github.com/mrghasemi1992" className="pointer">
         <img
           src={color === 'egyptianBlue' ? github : githubWhite}
           alt="GitHub"
           width={25}
         />
-      </Icon>
+      </a>
     </IconsWrapper>
   );
 }
